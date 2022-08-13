43d35de39a1cb4a9ad7b31d17f520fe7d40ea76e
izzanka <izzancoc.46@gmail.com>
2022-08-13 22:28:21 +0700

    edit query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $events = Event::latest()->where('status',5)->paginate(4);
-        return view('home');
+        $events = Event::latest()->where('status',0)->paginate(4);
+        return view('home',compact('events'));
     }
 }
