8749a034ea9ae6fba856d6f7d216e28854539536
izzanka <izzancoc.46@gmail.com>
2022-08-14 16:49:30 +0700

    add start_date getter

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Event extends Model
 {
@@ -24,5 +26,12 @@ class Event extends Model
     public function getRouteKeyName()
     {
         return 'name';
+    }
+
+    public function startDate(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::createFromFormat('Y-m-d',$value)->format('d M Y')
+        );
     }
 }
